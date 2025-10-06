eb8aa98f1af40e19c3b36df4d6114eb5d7221022
ariel mazalan <arielmazalan11@gmail.com>
2025-10-06 19:29:57 -0300

    Id_api to genre added + getMoviesByGenre prototype method added

--- src/main/java/entity/Genre.java
+++ src/main/java/entity/Genre.java
@@ -4,6 +4,7 @@ public class Genre {
 	
 	int id;
 	String name;
+	int id_api;
 	
 	public int getId() {
 		return id;
@@ -17,5 +18,11 @@ public class Genre {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public int getId_api() {
+		return id_api;
+	}
+	public void setId_api(int id_api) {
+		this.id_api = id_api;
+	}
+		
 }
